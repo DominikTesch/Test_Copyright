7d023fed77098e1cf174cdb54dfb56a35ecf59ef
Dominik Tesch <tesch@itemis.de>
2016-08-30 14:10:40 +0200

    test

---
+++ Test/src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2013 Dominik - All rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * 
+ * Contributors:
+ * 	Dominik
+ * 
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello World!");
+	}
+
+}
